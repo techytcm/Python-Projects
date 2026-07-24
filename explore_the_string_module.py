794a901ad791b8f4bcfdb553ec047711919d4d95
Tonmoy Chandra Mudi <tcm9798@gmail.com>
2026-07-24 21:53:51 +0600

    Explore the string Module

---
+++ explore_the_string_module.py
@@ -0,0 +1,13 @@
+import string
+
+print("Lowercase:")
+print(string.ascii_lowercase)
+
+print("Uppercase:")
+print(string.ascii_uppercase)
+
+print("Digits:")
+print(string.digits)
+
+print("Punctuation:")
+print(string.punctuation)
